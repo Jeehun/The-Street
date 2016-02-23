d31d7cecafaf4e7ac2b5240d8d9452f87e152f4f
jeehun <jeehun@naver.com>
2016-02-23 15:51:33 +0900

    header 수정

--- Real/header.php
+++ Real/header.php
@@ -5,6 +5,6 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="description" content="대학가/거리/시장의 상권 정보 제공 및 소상공인 무료 모두 홈페이지 제작/관리/교육 싸이트 '그 거리 뭐 있소'">
   <meta name="viewport" content="widht=deveice-width,initial-scale=0.6">
-  <title>그 거리 뭐 있소?</title>
+  <title> 그 거리 뭐 있소? </title>
   <link rel="stylesheet" type="text/css" href="style.css" />
 </head>
