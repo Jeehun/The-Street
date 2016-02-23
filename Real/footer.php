628013409bee3af49d3456d962a9e14b125d5368
jeehun <jeehun@naver.com>
2016-02-23 15:49:16 +0900

    Project_Start_01

---
+++ Real/footer.php
@@ -0,0 +1,4 @@
+<hr />
+<p class="footer">Copyright &copy;2016 무아지경 Inc.</p>
+</body>
+</html>
---
+++ Real/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="kor" style="overflow: hidden; height: 100%;">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="description" content="대학가/거리/시장의 상권 정보 제공 및 소상공인 무료 모두 홈페이지 제작/관리/교육 싸이트 '그 거리 뭐 있소'">
+  <meta name="viewport" content="widht=deveice-width,initial-scale=0.6">
+  <title>그 거리 뭐 있소?</title>
+  <link rel="stylesheet" type="text/css" href="style.css" />
+</head>
